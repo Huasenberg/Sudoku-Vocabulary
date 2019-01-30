5673322aeda200cf23c30cce68f9711adb754a9e
aghua <alex_hua@sfu.ca>
2019-01-29 16:21:00 -0800

    added main menu

--- app/src/main/java/ca/cmpt276theta/sudokuvocabulary/GameData.java
+++ app/src/main/java/ca/cmpt276theta/sudokuvocabulary/GameData.java
@@ -36,27 +36,28 @@ public class GameData {
         };
 
         // Pairing the words with numbers
-        Pair<Integer, String> pair1 = new Pair<>(1, "Pomme");
-        Pair<Integer, String> pair2 = new Pair<>(2, "Poir");
-        Pair<Integer, String> pair3 = new Pair<>(3, "Fraise");
-        Pair<Integer, String> pair4 = new Pair<>(4, "Ananans");
-        Pair<Integer, String> pair5 = new Pair<>(5, "Banane");
-        Pair<Integer, String> pair6 = new Pair<>(6, "Canneberge");
-        Pair<Integer, String> pair7 = new Pair<>(7, "Figue");
-        Pair<Integer, String> pair8 = new Pair<>(8, "Pêche");
-        Pair<Integer, String> pair9 = new Pair<>(9, "Cerise");
+        Pair<Integer, String> pair1 = new Pair<>(1, "mangue");
+        Pair<Integer, String> pair2 = new Pair<>(2, "poire");
+        Pair<Integer, String> pair3 = new Pair<>(3, "fraise");
+        Pair<Integer, String> pair4 = new Pair<>(4, "orange");
+        Pair<Integer, String> pair5 = new Pair<>(5, "banane");
+        Pair<Integer, String> pair6 = new Pair<>(6, "kiwi");
+        Pair<Integer, String> pair7 = new Pair<>(7, "prune");
+        Pair<Integer, String> pair8 = new Pair<>(8, "pêche");
+        Pair<Integer, String> pair9 = new Pair<>(9, "cerise");
 
         Pair mapping_array[] = { pair1, pair2, pair3, pair4, pair5,
                 pair6, pair7, pair8, pair9 };
 
         for(int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if puzzle[i][j] != 0 {
+                if (puzzle[i][j] != 0) {
                     for (int k = 0; k < 9; k++){
                         Pair<Integer, String> temp_pair = mapping_array[k];
                         int key = temp_pair.first;
                         if (puzzle[i][j] == key) {
-                            gridContent[i][j] = temp_pair.second();
+                            gridContent[i][j] = temp_pair.second;
+//                            System.out.println(temp_pair.second);
                         }
                     }
                 }
