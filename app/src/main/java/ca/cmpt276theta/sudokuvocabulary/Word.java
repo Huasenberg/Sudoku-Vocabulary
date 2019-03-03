8dec735d4fcbe01a1759196b1bb06e7cc531823a
aghua <alex_hua@sfu.ca>
2019-03-03 01:46:04 -0800

    implemented read from csv file

---
+++ app/src/main/java/ca/cmpt276theta/sudokuvocabulary/Word.java
@@ -0,0 +1,40 @@
+package ca.cmpt276theta.sudokuvocabulary;
+
+class Word {
+    private int wordID;
+    private String english;
+    private String french;
+    private int score;
+
+    public int getWordID() {
+        return wordID;
+    }
+
+    public void setWordID(int wordID) {
+        this.wordID = wordID;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+
+    public String getFrench() {
+        return french;
+    }
+
+    public void setFrench(String french) {
+        this.french = french;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
