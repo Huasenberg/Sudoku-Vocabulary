48032ae70abbb559d4d4c18e6d5c1b31e2d5afcd
Yiming Zhang <yza440@sfu.ca>
2019-04-13 19:00:14 -0700

    implement Unit Test

--- app/src/test/java/ca/cmpt276theta/sudokuvocabulary/model/GameDataListTest.java
+++ app/src/test/java/ca/cmpt276theta/sudokuvocabulary/model/GameDataListTest.java
@@ -11,20 +11,21 @@ public class GameDataListTest {
 
     @Test
     public void getGameDataList() {
-        List<GameData> gameDataList = new ArrayList<>();
-        gameDataList.add(new GameData(0,true,0));
-        GameDataList.setGameDataList(gameDataList);
-        assertEquals(gameDataList, GameDataList.getGameDataList());
+        GameDataList.getGameDataList().add(new GameData(0,true,0));
+        assertEquals(1, GameDataList.getGameDataList().size());
+        GameDataList.getGameDataList().add(new GameData(1,false,0));
+        GameDataList.getGameDataList().add(new GameData(2,true,2));
+        GameDataList.getGameDataList().add(new GameData(1,true,0));
+        GameDataList.getGameDataList().add(new GameData(0,false,0));
+        assertEquals(3, GameDataList.getGameDataList().size());
 
     }
 
     @Test
     public void setGameDataList() {
-
-        GameDataList.getGameDataList().add(new GameData(1, false, 2));
-        GameDataList.getGameDataList().add(new GameData(0, false, 1));
-        GameDataList.getGameDataList().add(new GameData(0, false, 1));
-        GameDataList.getGameDataList().add(new GameData(0, false, 1));
-        assertEquals(3, GameDataList.getGameDataList().size());
+        List<GameData> gameDataList = new ArrayList<>();
+        gameDataList.add(new GameData(0,true,0));
+        GameDataList.setGameDataList(gameDataList);
+        assertEquals(gameDataList, GameDataList.getGameDataList());
     }
 }
